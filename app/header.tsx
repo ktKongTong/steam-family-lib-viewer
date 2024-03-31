9ac2e3860d5ccc7fa486d1aa275765298bb74a88
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-03-31 22:00:01 +0800

    init

--- app/header.tsx
+++ app/header.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import {Github} from "lucide-react";
+import {gitlink} from "@/constant";
 
 export default function Header({
 
@@ -10,7 +11,7 @@ export default function Header({
     <header className={"w-full flex justify-between items-center h-16 px-20 py-4"}>
       <h1 className={' text-xl'}>steam 赛博家庭库存查看器</h1>
 
-      <Link href={''}>
+      <Link href={gitlink}>
         <Github className={'h-8 w-8'}/>
       </Link>
     </header>

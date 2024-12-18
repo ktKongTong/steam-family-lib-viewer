711a608b6f8efb9629fcb4a9e870bb63decd6007
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-12-18 10:51:38 +0800

    fix(steam-proto): export createSteamWebStdAPI function

--- packages/steam-proto/src/api/index.ts
+++ packages/steam-proto/src/api/index.ts
@@ -1,4 +1,4 @@
 
-export { steamWebStdAPI } from './web'
+export { steamWebStdAPI, createSteamWebStdAPI } from './web'
 export * from './gen/types'
 export type { InferReqType, InferRespType, InferReqJsonType, InferRespJsonType, SteamStdResponseJsonType,SteamStdRequestJsonType, SteamStdResponseType, ServiceDict, ServiceMethodDict }  from './types'
